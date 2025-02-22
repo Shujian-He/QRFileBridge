a8b0e403d566996637bbd0817634ebf469a652ce
Shujian <hesj@outlook.jp>
2025-02-22 13:42:56 +0800

    Initial Commit

---
+++ QRFileBridge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QRFileBridge
+//
+//  Created by Shujian He on 22/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QRFileBridge/QRFileBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRFileBridgeApp.swift
+//  QRFileBridge
+//
+//  Created by Shujian He on 22/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct QRFileBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
